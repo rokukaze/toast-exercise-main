53d02978d1c9a2007d6112a7fe593be72417b6fb
Naz <17075595+rokukaze@users.noreply.github.com>
2023-05-02 09:22:33 -0700

    Added primary and secondary colours to entire app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import Container from '@mui/material/Container';
+import React from "react";
+import Container from "@mui/material/Container";
 
-import Header from './Header';
-import Content from './Content';
+import Header from "./Header";
+import Content from "./Content";
+
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4535b0",
+    },
+    secondary: {
+      main: "#fe0d4b",
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <Container>
-        <Content />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Container>
+          <Content />
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
