04c21e506a55e62690a0b552c1b2491b4c20a122
Naz <17075595+rokukaze@users.noreply.github.com>
2023-05-02 09:47:43 -0700

    Added snackbar, button implementations and proper css

--- src/Header.js
+++ src/Header.js
@@ -1,40 +1,91 @@
-import React from 'react';
+import { React, useState } from "react";
 
-import Box from '@mui/material/Box';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Snackbar } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
-import { createMockFormSubmission } from './service/mockServer';
+// Removed until basic functionality of toast popup implemented
+// import { createMockFormSubmission } from "./service/mockServer";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <>
+      <Button
+        style={{ color: "aqua" }}
+        size="small"
+        onClick={() => {
+          // submit like to backend here
+          setOpen(false);
+        }}
+      >
+        Like
+      </Button>
+      <IconButton
+        sx={{ color: "white" }}
+        onClick={() => {
+          handleClose();
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
+    </>
+  );
+
   return (
-    <Box sx={{flexGrow: 1}}>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <IconButton
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{marginRight: 2}}
+            sx={{ marginRight: 2 }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" sx={{flexGrow: 1}}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Toast Exercise
           </Typography>
           <Button
             variant="contained"
             size="small"
             color="secondary"
-            onClick={() => createMockFormSubmission()}
+            // Needs proper implementation
+            // add mock form submission back into handle click, but needs to execute something before hand, onMessage allows some kind of function execution
+            // use onMessage to execute a function that i create, related to saveLikedFormSubmission method
+            onClick={handleClick}
           >
             New Submission
           </Button>
         </Toolbar>
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          onClose={handleClose}
+          message="Toast Info Here"
+          action={action}
+        />
       </AppBar>
     </Box>
   );
